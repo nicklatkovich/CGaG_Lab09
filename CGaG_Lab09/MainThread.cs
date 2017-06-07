5f0aba94698899b3ae406297bbedeb1b7ad5b52d
NickLatkovich <nicklatkovich@gmail.com>
2017-06-07 10:05:45 +0300

    Init

---
+++ CGaG_Lab09/MainThread.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CGaG_Lab09 {
+    public class MainThread : Game {
+        GraphicsDeviceManager Graphics;
+        SpriteBatch SpriteBatch;
+
+        public MainThread( ) {
+            Graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize( ) {
+            // TODO: Add initialization logic
+
+            base.Initialize( );
+        }
+
+        protected override void LoadContent( ) {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            SpriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load game content
+        }
+
+        protected override void UnloadContent( ) {
+            // TODO: Unload any non ContentManager content
+        }
+
+        protected override void Update(GameTime time) {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState( ).IsKeyDown(Keys.Escape))
+                Exit( );
+
+            // TODO: Add update logic
+
+            base.Update(time);
+        }
+
+        protected override void Draw(GameTime time) {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add drawing code
+
+            base.Draw(time);
+        }
+    }
+}
---
+++ CGaG_Lab09/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CGaG_Lab09 {
+#if WINDOWS || LINUX
+    public static class Program {
+        [STAThread]
+        static void Main( ) {
+            using (var game = new MainThread( ))
+                game.Run( );
+        }
+    }
+#endif
+}
